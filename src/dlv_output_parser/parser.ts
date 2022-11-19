b7c0fccc20622be0ec5e6a85b9315ad998e7cd54
fedcal <gunere3@gmail.com>
2022-11-19 12:39:51 +0100

    added write_to_file of model capability

--- src/dlv_output_parser/parser.ts
+++ src/dlv_output_parser/parser.ts
@@ -1,9 +1,13 @@
 import { Output } from "./models/output";
 import * as fs from 'fs';
+import { DlvModel } from "./interfaces/dlv_model";
 
 export class Parser {
     public static parse_output_file(path: string): Output {
-        const file: string = fs.readFileSync(path, 'utf-8')
+        const file: string = fs.readFileSync(path, {encoding: 'utf-8'})
         return Output.parse(file) as Output
+    }
+    public static write_to_file(path: string, model : DlvModel) {
+        return fs.writeFileSync(path, model.stringify(), {encoding: 'utf-8', flag: 'w'})
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import { Parser } from "./dlv_output_parser/parser"
 
 let output : Output = Parser.parse_output_file('./assets/LoIDE_output (2).txt')
 console.log(output.stringify())
+// Parser.write_to_file("test.txt", output)

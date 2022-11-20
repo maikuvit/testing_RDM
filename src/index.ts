d4eaecc2297b5959db48d43038f9765dfca2950c
Giacomo Galavotti <g.galavotti@disroot.org>
2022-11-20 12:12:38 +0100

    simple example script for getting program from...
    .file path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import { Output } from "./dlv_output_parser/models/output"
 import { Parser } from "./dlv_output_parser/parser"
 
-let output : Output = Parser.parse_output_file('./assets/LoIDE_output (5).txt')
+//get argv parameters
+var args = process.argv
+
+//remove execution paths
+for (let i = 0; i < 2; i++) {
+    args.shift()
+}
+console.log(args)
+
+//get program path arg
+let file = args.shift();
+let output: Output;
+
+if (file !== undefined) {
+    output = Parser.parse_output_file(file);
+}
+else {
+    output = Parser.parse_output_file('./assets/LoIDE_output (5).txt')
+}
 console.log(output)
 console.log(output.stringify())
 //console.log(output.answers[0].stringify())

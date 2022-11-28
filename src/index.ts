549b7b7c533feee9287fcb1d5dab71329017249b
Federico Calabrò <gunere3@gmail.com>
2022-11-28 12:00:27 +0100

    added invoke incomplete cli

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,35 @@
 import * as figlet from "figlet";
-import { Command } from 'commander';
+import { Command, Option } from "commander";
 import { Parser } from "./dlv_output_parser/parser";
 
-console.log(figlet.textSync("TASPER"))
+console.log(figlet.textSync("TASPER"));
 const program = new Command();
 
 program
-  .name('tasper')
-  .description('CLI to ASP testing framework')
-  .version('1.0.0', '-v, --version', 'Output the current version');
+  .name("tasper")
+  .description("CLI to ASP testing framework")
+  .version("1.0.0", "-v, --version", "Output the current version");
 
-program.command('parse')
-  .description('Parse an ASP output file')
-  .argument('<path>', 'Path to file')
+program
+  .command("parse")
+  .description("Parse an ASP output file")
+  .argument("<path>", "Path to file")
   .action((path) => {
-    let output = Parser.parse_output_file(path)
+    let output = Parser.parse_output_file(path);
     console.log(output);
-    console.log(output.stringify())
+    console.log(output.stringify());
   });
+
+  
+program
+.command("invoke")
+.description("Invoke solver for input file")
+.argument("<path>", "Path to file")
+.addOption(new Option('-s, --solver <solver>', 'invoke specified solver').choices(['dlv2']).makeOptionMandatory())
+.action((path, options) => {
+  console.log(options);
+  console.log(path);
+});
 
 program.parse(process.argv);
 

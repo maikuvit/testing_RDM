a71a91be38bc799edd590e4d2f14c2a662bd3ec5
maikuvit <michelevitale000@gmail.com>
2022-11-29 13:48:52 +0100

    defined solver structure

--- src/test_solver/TestSolver.ts
+++ src/test_solver/TestSolver.ts
@@ -1,5 +1,31 @@
+import { DlvModel } from "../dlv_output_parser/interfaces/dlv_model";
+import { Output } from "../dlv_output_parser/models/output";
+import { TestInterface } from "../testing_module/interfaces/testInterface";
 
+// ----maiku---- //
 export class TestSolver {
-    
-    
+
+    //ho una obj test che contiene tutte le info:
+    // mi serve: 
+    // public met solve <- input Test
+    // private met exec_solver <- set di opzioni sul solver
+    //                           path file di input
+    //                           parse su input (modulo di fede)
+    // private met genFile <- set input
+
+    //temp implementation!
+    public solve(Test : TestInterface) : boolean {
+        return true;
+    }
+
+    //temp implementation!
+    private exec_solver() : DlvModel{
+        return new Output([]);
+    } 
+
+    //temp implementation!
+    private genTempFile() : String{
+        return "";
+    }
+
 }

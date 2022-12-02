74515ce4e0908c2af4bd9686a18121c93e9c9dcf
Lebonsky <gionnym@icloud.com>
2022-12-02 23:45:18 +0100

    Added assertsTrue implementations and tests

--- src/asserts/interfaces/dlvAssert.ts
+++ src/asserts/interfaces/dlvAssert.ts
@@ -1,6 +1,23 @@
 import { DlvModel } from "../../dlv_output_parser/interfaces/dlv_model";
+import { AnswerSet } from "../../dlv_output_parser/models/answer_set";
+import { Atom } from "../../dlv_output_parser/models/atom";
+import { arrayContainsAll } from "../../utils/utils";
 
 export abstract class DlvAssert {
+    
+    public abstract validate(model : DlvModel) : boolean
+}
+
+export abstract class AssertTrueIn {
+    
+    public constructor(
+        public atoms: Atom[],
+    ){}
+
+    protected true_in_one(ans: AnswerSet): boolean{
+        return arrayContainsAll(ans.atoms, this.atoms)
+    }
 
     public abstract validate(model : DlvModel) : boolean
+
 }

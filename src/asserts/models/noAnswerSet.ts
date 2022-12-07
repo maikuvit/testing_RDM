d111d1bb55d0d4b800bb3441bf932c0d2cd70daf
FabioBarrese <fbarrese96@hotmail.com>
2022-12-07 21:46:37 +0100

    some adjustment

--- src/asserts/models/noAnswerSet.ts
+++ src/asserts/models/noAnswerSet.ts
@@ -3,14 +3,14 @@ import { Output } from "../../dlv_output_parser/models/output";
 import { DlvAssert } from "../interfaces/dlvAssert";
 
 export class NoAnswerSet extends DlvAssert {
-    public PreConditions(): object {
+    public PreConditions(): any {
         return {};
     }
-    protected FullfilRequirements(model : Output): DlvModel {
+    protected override FullfilRequirements(model : Output): DlvModel {
         // no requirements at all ...
         return model;
     }
-    protected validate(output: Output): boolean {
+    protected override validate(output: Output): boolean {
 
         return output.answers.length === 0;
     }
--- src/asserts/models/trueInExactly.ts
+++ src/asserts/models/trueInExactly.ts
@@ -1,16 +1,14 @@
 import { DlvModel } from "../../dlv_output_parser/interfaces/dlv_model";
-import { AnswerSet } from "../../dlv_output_parser/models/answer_set";
-import { Atom } from "../../dlv_output_parser/models/atom";
 import { Output } from "../../dlv_output_parser/models/output";
 import { convertedAtoms } from "../../utils/utils";
 import { DlvAssert } from "../interfaces/dlvAssert";
 
 export class TrueInExactly extends DlvAssert {
-    protected FullfilRequirements(model: DlvModel): DlvModel {
+    protected override FullfilRequirements(model: DlvModel): DlvModel {
         return model;
     }
 
-    public PreConditions(): object {
+    public PreConditions(): any {
         return {
             "input": [""],
             "options" : ["-n0"]
@@ -24,7 +22,7 @@ export class TrueInExactly extends DlvAssert {
         super()
     }
 
-    public override validate(output: Output): boolean {
+    protected override validate(output: Output): boolean {
         let count = 0
         for(let j=0;j < output.answers.length;j++){
             if(output.answers[j].containsAllAtoms(convertedAtoms(this.atoms))){

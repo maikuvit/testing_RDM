853e6fb6c3b9c07cf4001bb73c873470278acddb
fedcal <gunere3@gmail.com>
2022-12-09 23:25:50 +0100

    common folder with interfaces and utils

---
+++ src/common/interfaces/assert.ts
@@ -0,0 +1,15 @@
+import { DlvOutputModel } from "./dlv_model";
+
+export abstract class Assert {
+
+    //Asserts can require different conditions to be satisfied, that will be implemented in this method 
+    protected abstract fullfilRequirements(model: DlvOutputModel): DlvOutputModel
+
+    protected abstract validate(model: DlvOutputModel): boolean
+
+    public assert(model: DlvOutputModel): boolean {
+        return this.validate(this.fullfilRequirements(model));
+    }
+
+    public abstract preConditions(): any
+}

c818f1687f75ce28a5d3d864023ea231cf64630e
FabioBarrese <fbarrese96@hotmail.com>
2022-12-13 14:52:22 +0100

    TrueInExactly conform to the new interface

--- src/test_solver/asserts/true_in_exactly.ts
+++ src/test_solver/asserts/true_in_exactly.ts
@@ -1,5 +1,6 @@
 import { Assert } from "../../common/interfaces/assert";
 import { DlvOutputModel } from "../../common/interfaces/dlv_model";
+import { preConditions } from "../../common/pre_conditions";
 import { arrayContainsAll } from "../../common/utils";
 import { Atom } from "../../dlv_output_parser/models/atom";
 import { Output } from "../../dlv_output_parser/models/output";
@@ -13,18 +14,16 @@ export class TrueInExactly extends Assert {
         super()
     }
 
-    public override fullfilRequirements(model: DlvOutputModel): DlvOutputModel {
-        return model;
+    public fullfilRequirements(model: DlvOutputModel): [DlvOutputModel] {
+        // qui devo generare un 
+        return [model];
     }
 
-    public preConditions(): any {
-        return {
-            "input": [""],
-            "options": ["-n0"]
-        };
+    public preConditions(): preConditions {
+        return new preConditions([""], "-n0", false);
     }
 
-    public override validate(output: Output): boolean {
+    public override assert(output: Output): boolean {
         let count = 0
         output.answers.forEach(ans => count += arrayContainsAll(ans.atoms, this.atoms) ? 1 : 0)
         return count === this.number

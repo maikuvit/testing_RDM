68731672b7b0e419ae2193e852ce41fdd49cf69e
fedcal <gunere3@gmail.com>
2023-01-29 17:53:50 +0100

    fix one test

--- test/test_solver/TestSolver.ts
+++ test/test_solver/TestSolver.ts
@@ -7,7 +7,7 @@ import { TestParser } from '../../src/test_parser/test_parser';
 describe("The solver works properly", () => {
 
     it("should solve a basic example", () => {
-        TestParser.parse_test_file("assets/input2_tests.tasp").tests.forEach(async (test) => {
+        TestParser.parse_test_file("demo/input2.asp").tests.forEach(async (test) => {
             let out = await TestSolver.solve(test, 'dlv2');
             assert.equal(JSON.stringify(out) == JSON.stringify({ 0: true }), false);
         })

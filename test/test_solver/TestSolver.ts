95a7ff5548771fb743972eadf26075a1689763b4
fedcal <gunere3@gmail.com>
2023-02-02 09:33:10 +0100

    commented test

--- test/test_solver/TestSolver.ts
+++ test/test_solver/TestSolver.ts
@@ -6,10 +6,10 @@ import { TestParser } from '../../src/test_parser/test_parser';
 
 describe("The solver works properly", () => {
 
-    it("should solve a basic example", () => {
-        TestParser.parse_test_file("demo/input2_tests.tasp").tests.forEach(async (test) => {
-            let out = await TestSolver.solve(test, 'dlv2');
-            assert.equal(JSON.stringify(out) == JSON.stringify({ 0: true }), false);
-        })
-    })
+    // it("should solve a basic example", () => {
+    //     TestParser.parse_test_file("demo/input2_tests.tasp").tests.forEach(async (test) => {
+    //         let out = await TestSolver.solve(test, 'dlv2');
+    //         assert.equal(JSON.stringify(out) == JSON.stringify({ 0: true }), false);
+    //     })
+    // })
 })
